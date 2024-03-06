bd2bc5cf2c2a01a7ca48565b0126b3fbd64e5eca
Mingyu Liu <mingyuliu88@gmail.com>
2024-03-06 16:15:05 -0500

    Refactor UI for editing journal entry

--- app/src/main/java/ca/uwaterloo/treklogue/ui/profile/JournalEntryDetail.kt
+++ app/src/main/java/ca/uwaterloo/treklogue/ui/profile/JournalEntryDetail.kt
@@ -2,13 +2,18 @@ package ca.uwaterloo.treklogue.ui.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,13 +21,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
@@ -32,6 +37,8 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import ca.uwaterloo.treklogue.R
 import ca.uwaterloo.treklogue.data.mockModel.MockJournalEntry
+import ca.uwaterloo.treklogue.ui.theme.Blue100
+import ca.uwaterloo.treklogue.ui.theme.Blue200
 
 @Composable
 fun JournalEntryDetail(
@@ -63,39 +70,42 @@ fun TopBar(onBackClicked: () -> Unit) {
         IconButton(
             onClick = { onBackClicked() },
         ) {
-            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+            Icon(Icons.Filled.ArrowBack, contentDescription = stringResource(R.string.save))
         }
         Spacer(modifier = Modifier.weight(1f))
         Button(onClick = {/* TODO: handle save */ }) {
-            Text(stringResource(R.string.save))
+            Text(stringResource(R.string.save), style = MaterialTheme.typography.labelLarge)
         }
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ContentSection(journalEntry: MockJournalEntry) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(12.dp, 0.dp, 12.dp, 12.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         OutlinedTextField(
+            readOnly = true,
+            enabled = false,
             value = TextFieldValue(journalEntry.name),
-            onValueChange = { /* TODO: handle text change */ },
+            onValueChange = {},
             label = { Text(stringResource(R.string.name_of_landmark)) },
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
         OutlinedTextField(
+            readOnly = true,
+            enabled = false,
             value = TextFieldValue(journalEntry.dateVisited),
-            onValueChange = { /* TODO: handle text change */ },
+            onValueChange = {},
             label = { Text(stringResource(R.string.date_of_visit)) },
             modifier = Modifier.fillMaxWidth()
         )
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
             value = TextFieldValue(journalEntry.description),
@@ -106,34 +116,32 @@ fun ContentSection(journalEntry: MockJournalEntry) {
                 .height(150.dp),
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(
-            modifier = Modifier
-                .height(150.dp)
-                .fillMaxWidth()
-                .border(1.dp, Color.Gray, RectangleShape)
-                .clip(RectangleShape)
-                .weight(1f)
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             journalEntry.images.forEach {
                 Image(
                     painter = painterResource(id = it),
                     contentDescription = null,
-                    modifier = Modifier.matchParentSize(),
-                    contentScale = ContentScale.Fit
+                    modifier = Modifier
+                        .height(150.dp)
+                        .border(1.dp, Color.Gray, RectangleShape),
+                    contentScale = ContentScale.Fit,
                 )
             }
 
             FloatingActionButton(
                 onClick = { /* TODO: handle add image */ },
                 modifier = Modifier
-                    .align(Alignment.BottomEnd)
-                    .padding(10.dp),
+                    .height(75.dp)
+                    .width(75.dp)
+                    .align(Alignment.CenterVertically),
+                containerColor = Blue100,
+                contentColor = Blue200
             ) {
                 Icon(Icons.Filled.Add, contentDescription = "Add")
             }
         }
-        Spacer(modifier = Modifier.height(8.dp))
     }
 }
