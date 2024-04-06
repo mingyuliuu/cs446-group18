f93fa964d50b4ea60320c837fae68967cbf6a3c3
Mingyu Liu <mingyuliu88@gmail.com>
2024-04-06 00:58:53 -0400

    Style: Update loading popup

--- app/src/main/java/ca/uwaterloo/treklogue/ui/composables/LoadingPopup.kt
+++ app/src/main/java/ca/uwaterloo/treklogue/ui/composables/LoadingPopup.kt
@@ -2,13 +2,20 @@ package ca.uwaterloo.treklogue.ui.composables
 
 import android.Manifest
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ca.uwaterloo.treklogue.ui.theme.Gray100
 import ca.uwaterloo.treklogue.ui.viewModels.MapViewModel
@@ -32,16 +39,30 @@ fun LoadingPopup(
     val coarseLocationPermissionState = rememberPermissionState(coarseLocationPermission)
 
     if (mapViewModel.state.value.userLocation == LatLng(
-            43.4822734, -80.5879188)
+            43.4822734, -80.5879188
+        )
     ) {
-        Box(modifier = Modifier.fillMaxSize().background(Gray100)) {
-            Row(modifier = Modifier.align(Alignment.Center), verticalAlignment = Alignment.CenterVertically) {
-                Text(text =
-                if (!locationPermissionState.status.isGranted && !coarseLocationPermissionState.status.isGranted)
-                    "Please allow location permissions..."
-                else "Retrieving initial location...", style = MaterialTheme.typography.headlineLarge)
-                Spacer(Modifier.width(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Gray100)
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 CircularProgressIndicator()
+                Spacer(Modifier.height(20.dp))
+                Text(
+                    textAlign = TextAlign.Center,
+                    text =
+                    if (!locationPermissionState.status.isGranted && !coarseLocationPermissionState.status.isGranted)
+                        "Please allow location permissions..."
+                    else "Retrieving initial location...",
+                    style = MaterialTheme.typography.headlineLarge
+                )
             }
         }
     }
