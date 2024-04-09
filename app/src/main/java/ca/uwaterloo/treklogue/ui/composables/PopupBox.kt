dbcadea12446f4d62bab86ef8f50b1475380570a
Mingyu Liu <mingyuliu88@gmail.com>
2024-04-08 22:05:20 -0400

    Style: Refactor terms and conditions pop up

--- app/src/main/java/ca/uwaterloo/treklogue/ui/composables/PopupBox.kt
+++ app/src/main/java/ca/uwaterloo/treklogue/ui/composables/PopupBox.kt
@@ -1,45 +1,32 @@
 package ca.uwaterloo.treklogue.ui.composables
 
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
 import androidx.compose.ui.zIndex
 
-
 @Composable
-fun PopupBox(popupWidth: Float, popupHeight:Float, showPopup: Boolean, onClickOutside: () -> Unit, content: @Composable() () -> Unit) {
-
+fun PopupBox(
+    widthFraction: Float,
+    heightFraction: Float,
+    showPopup: Boolean,
+    onClickOutside: () -> Unit,
+    content: @Composable () -> Unit
+) {
     if (showPopup) {
-        // full screen background
+        // Full screen background
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -47,7 +34,6 @@ fun PopupBox(popupWidth: Float, popupHeight:Float, showPopup: Boolean, onClickOu
                 .zIndex(10F),
             contentAlignment = Alignment.Center
         ) {
-            // popup
             Popup(
                 alignment = Alignment.Center,
                 properties = PopupProperties(
@@ -58,17 +44,14 @@ fun PopupBox(popupWidth: Float, popupHeight:Float, showPopup: Boolean, onClickOu
             ) {
                 Box(
                     Modifier
-                        .width(popupWidth.dp)
-                        .height(popupHeight.dp)
+                        .fillMaxWidth(widthFraction)
+                        .fillMaxHeight(heightFraction)
+                        .clip(RoundedCornerShape(12.dp))
                         .background(Color.White)
-                        .clip(RoundedCornerShape(4.dp)),
+                        .padding(20.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    Column(
-                        modifier = Modifier.verticalScroll(rememberScrollState())
-                    ) {
-                        content()
-                    }
+                    content()
                 }
             }
         }
